28979eafbe173f47bcee861466f8eb2ddba7a4ee
soritesparadox <48735681+soritesparadox@users.noreply.github.com>
2022-08-13 13:58:46 -0400

    Update codepen setup.

--- docs/ayva-codepen-setup.js
+++ docs/ayva-codepen-setup.js
@@ -1,16 +1,13 @@
-import OSREmulator from 'https://unpkg.com/osr-emu';
-import * as AyvaModules from 'https://unpkg.com/ayvajs@0.13.3';
-
 const element = document.createElement('div');
 element.id = 'canvas';
 element.style['height'] = '100vh';
 document.body.appendChild(element);
 
-const ayva = new AyvaModules.Ayva().defaultConfiguration();
+const ayva = new Ayva().defaultConfiguration();
 ayva.addOutputDevice(new OSREmulator(element));
 
 ayva.updateLimits('stroke', 0.25, 0.75);
 ayva.updateLimits('roll', 0.25, 0.75);
 ayva.updateLimits('pitch', 0.25, 0.75);
 
-Object.assign(window, AyvaModules, { ayva });
+Object.assign(window, { ayva });
